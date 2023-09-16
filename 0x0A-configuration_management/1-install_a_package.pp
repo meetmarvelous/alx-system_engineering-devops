723aa254691ca32879089a59d36931e30da9db4b
meetmarvelous <marvel1698@gmail.com>
2023-09-16 14:02:50 +0100

    system

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Installing a package using puppet
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,4 +0,0 @@
-# Executing commands with puppet
-exec {'kill_process':
-  command => '/usr/bin/pkill -f killmenow',
-}
